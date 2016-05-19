b161211391594754d4e4d2bf85120dfdfe672fc6
Your Name <you@example.com>
2016-05-19 11:24:01 +0000

    more function

--- installme.sh
+++ installme.sh
@@ -6,3 +6,5 @@ sudo wget http://adafruit-download.s3.amazonaws.com/adafruit_hostapd_14128.zip
 sudo tar xvfz 1610-088.tgz
 sudo cp 1610-088/hostapd.conf /etc/hostapd
 sudo cp hostapd /etc/default/hostapd
+sudo rm -f 1610-088.tgz
+sudo rm -f adafruit_hostapd_14128.zip
