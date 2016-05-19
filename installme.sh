fbd825cb764128d1ecb9e708e628d7398c8904a0
Your Name <you@example.com>
2016-05-19 11:10:28 +0000

    new files

--- installme.sh
+++ installme.sh
@@ -3,4 +3,6 @@ sudo cp interfaces /etc/network/interfaces
 sudo apt-get install hostapd
 sudo wget ftp://ftp.heise.de/pub/ct/listings/1610-088.tgz
 sudo wget http://adafruit-download.s3.amazonaws.com/adafruit_hostapd_14128.zip
-tar xvfz 1610-088.tgz
+sudo tar xvfz 1610-088.tgz
+sudo cp 1610-088/hostapd.conf /etc/hostapd
+sudo cp hostapd /etc/default/hostapd
