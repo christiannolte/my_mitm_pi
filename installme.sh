5f9a6fb6578c6ac6d53a76f9e21e7b1d0097b1ce
Your Name <you@example.com>
2016-05-20 18:46:50 +0000

    file renamed

--- installme.sh
+++ installme.sh
@@ -6,9 +6,9 @@ sudo wget ftp://ftp.heise.de/pub/ct/listings/1610-088.tgz
 sudo wget http://adafruit-download.s3.amazonaws.com/adafruit_hostapd_14128.zip
 sudo tar xvfz 1610-088.tgz
 sudo cp 1610-088/hostapd.conf /etc/hostapd
-sudo cp hostapd /etc/default/hostapd
+sudo cp hostapd_conf /etc/default/hostapd
 sudo rm -f 1610-088.tgz
-sudo rm -f adafruit_hostapd_14128.zip
+echo sudo rm -f adafruit_hostapd_14128.zip
 sudo cp sysctl.conf /etc/sysctl.conf
 sudo apt-get install dnsmasq
 sudo cp dnsmasq.conf /etc/dnsmasq.conf
